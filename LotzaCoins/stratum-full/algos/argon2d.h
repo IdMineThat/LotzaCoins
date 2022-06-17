a89659ecbf45d3828a54d804beea361217a796dc
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2022-06-17 00:29:02 -0500

    Update argon2d.h

--- LotzaCoins/stratum-full/algos/argon2d.h
+++ LotzaCoins/stratum-full/algos/argon2d.h
@@ -10,6 +10,7 @@ extern "C" {
 void argon2d_crds_hash(const char* input, char* output, unsigned int len);
 void argon2d_dyn_hash(const char* input, char* output, unsigned int len);
 void argon2d_uis_hash(const char* input, char* output, unsigned int len);
+void argon2d16000_hash(const char* input, char* output, unsigned int len);
 
 #ifdef __cplusplus
 }
