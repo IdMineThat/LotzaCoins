43f0f6ac2209484f42b9ab6692e3e38498806aca
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2022-06-20 21:25:29 -0500

    Update argon2d.c

--- LotzaCoins/stratum-lowdiff/algos/argon2d.c
+++ LotzaCoins/stratum-lowdiff/algos/argon2d.c
@@ -63,6 +63,32 @@ void argon2d_dyn_call(const void *input, void *output)
 	argon2_ctx(&context, Argon2_d);
 }
 
+void argon2d16000_call(const void *input, void *output)
+{
+    argon2_context context;
+    context.out = (uint8_t *)output;
+    context.outlen = (uint32_t)OUTPUT_BYTES;
+    context.pwd = (uint8_t *)input;
+    context.pwdlen = (uint32_t)INPUT_BYTES;
+    context.salt = (uint8_t *)input; //salt = input
+    context.saltlen = (uint32_t)INPUT_BYTES;
+    context.secret = NULL;
+    context.secretlen = 0;
+    context.ad = NULL;
+    context.adlen = 0;
+    context.allocate_cbk = NULL;
+    context.free_cbk = NULL;
+    context.flags = DEFAULT_ARGON2_FLAG; // = ARGON2_DEFAULT_FLAGS
+    // main configurable Argon2 hash parameters
+    context.m_cost = 16000; // Memory in KiB (~16384KB)
+    context.lanes = 1;    // Degree of Parallelism
+    context.threads = 1;   // Threads
+    context.t_cost = 1;    // Iterations
+	
+	
+	argon2_ctx(&context, Argon2_d);
+}
+
 void argon2d_crds_hash(const unsigned char* input, unsigned char* output, unsigned int len)
 {
 	argon2d_crds_call(input, output);
