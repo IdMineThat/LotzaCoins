857b092ab7d76b2b1cf6fac1525c97669a735f52
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2023-03-06 19:29:25 -0600

    Add neoscrypt-xaya

--- LotzaCoins/stratum-lowdiff/stratum.cpp
+++ LotzaCoins/stratum-lowdiff/stratum.cpp
@@ -168,6 +168,7 @@ YAAMP_ALGO g_algos[] =
 	{"minotaurx", minotaurx_hash, 1, 0, 0},
 	{"myr-gr", groestlmyriad_hash, 1, 0, 0}, /* groestl + sha 64 */
 	{"neoscrypt", neoscrypt_hash, 0x10000, 0, 0},
+	{"neoscrypt-xaya", neoscrypt_hash, 0x10000, 0, 0},
 	{"nist5", nist5_hash, 1, 0, 0},
 	{"pawelhash", pawelhash_hash, 0x100, 0, 0},
 	{"penta", penta_hash, 1, 0, 0},
