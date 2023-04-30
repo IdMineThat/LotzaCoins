d702aa26b6a5e4a338aaec43b98a6b699ab44ae8
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2023-04-29 22:05:03 -0500

    Add Sha512256d Algorithm [Radiant (RAD)]

--- LotzaCoins/stratum-lowdiff/stratum.h
+++ LotzaCoins/stratum-lowdiff/stratum.h
@@ -248,4 +248,5 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/anime.h"
 #include "algos/cosa.h"
 #include "algos/minotaurx.h"
+#include "algos/sha512_256.h"
 #include "algos/0x10.h"
